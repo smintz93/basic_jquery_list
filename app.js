a035c70b4e2071cea0e04a26c1626b41fac63969
Sam <sammintz1993@gmail.com>
2018-10-23 23:06:08 -0500

    input has to have input value

--- app.js
+++ app.js
@@ -6,8 +6,17 @@ $('.add-item').on("click", (e) => {
 	e.preventDefault();
     const listItem = $(".item-input").val();
 	const itemHtml = "<li><span class='item-check'></span><span class='item-text'>" + listItem + "</span><span class='item-remove'></span></li>";
-  	$(".shopping-list").prepend(itemHtml);
-  	$(".item-input").val("");
+  	// if item is an empty string then post a message saying it needs to be updated
+  	if(listItem === "") {
+  		// or message that says please enter a value 
+  		return null
+  	} else {
+  		 // else prepend it 
+	  	$(".shopping-list").prepend(itemHtml);
+	  	$(".item-input").val("");
+  	}
+
+  	
 })
 
 $('.shopping-list').on("click", '.item-remove', (e) => {
@@ -17,3 +26,4 @@ $('.shopping-list').on("click", '.item-remove', (e) => {
 $('.shopping-list').on("click", 'item-check', (e) => {
 	  $(event.currentTarget).toggleClass('complete');
 })
+
